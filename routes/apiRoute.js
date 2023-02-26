8fe6d61c377bbaa514c8d3311442525dbccc1191
JeenaKozhimukkath <jeenasan@jeenas-mbp.home>
2023-02-25 22:16:48 -0500

    resolved issues

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,10 +1,10 @@
 const db = require("../db/db.json");
 const fs = require("fs");
-//const fs = require("../db/db.json");
+const path = require('path');
 
 module.exports = (app) => {
     app.get("/api/notes", (req, res) => {
-        res.json(db);
+            res.sendFile(path.join(__dirname, '../db/db.json'));
     })
 
 
@@ -12,14 +12,16 @@ app.post("/api/notes", (req, res) => {
     const createNote = req.body;
     createNote.id = db.length + 1;
     db.push(createNote);
-    fs.writeFileSync("../db/db.json", JSON.stringify(db));
+    fs.writeFileSync("./db/db.json", JSON.stringify(db));
     res.json(db);
 });
 
 app.delete("/api/notes/:id", (req,res) => {
     const noteId = parseInt(req.params.id);
-    const dbObject = db.find( (data) => data.id === noteId)
-    fs.writeFileSync("../db/db.json", JSON.stringify(db));
+    let dbList = fs.readFileSync('./db/db.json');
+    dbList = JSON.parse(dbList);
+    const dbObject = dbList.filter( (data) => data.id !== noteId)
+    fs.writeFileSync("./db/db.json", JSON.stringify(dbObject));
     res.json(dbObject);
 })
 };
